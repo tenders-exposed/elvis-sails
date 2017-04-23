360dbd0e5ec450da8cfcdc1fe59df9bf8efe23e7
Victor Nițu <victor@thesponge.eu>
2017-04-23 12:57:35 +0300

    Bootstrapped some controllers

---
+++ api/controllers/ProcurerController.js
@@ -0,0 +1,11 @@
+/**
+ * ProcurerController
+ *
+ * @description :: Server-side logic for managing procurers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/SupplierController.js
@@ -0,0 +1,11 @@
+/**
+ * SupplierController
+ *
+ * @description :: Server-side logic for managing suppliers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
